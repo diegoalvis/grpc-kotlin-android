3c1ace6e46565013cc22625956ff3d8ca3d803f4
Diego Alvis <diegoalvispal@gmail.com>
2022-06-10 19:03:22 +0200

    Setup ice cream server

---
+++ grpc-kotlin/build.gradle.kts
@@ -0,0 +1,140 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21" apply false
+    id("com.google.protobuf") version "0.8.18" apply false
+
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+group = "io.grpc"
+version = "1.3.0" // CURRENT_GRPC_KOTLIN_VERSION
+
+ext["grpcVersion"] = "1.46.0"
+ext["protobufVersion"] = "3.20.1"
+ext["coroutinesVersion"] = "1.6.2"
+
+subprojects {
+
+    apply {
+        plugin("java")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("com.google.protobuf")
+        plugin("maven-publish")
+        plugin("signing")
+    }
+
+    // gradle-nexus/publish-plugin needs these here too
+    group = rootProject.group
+    version = rootProject.version
+
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+        targetCompatibility = JavaVersion.VERSION_1_8.toString()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = JavaVersion.VERSION_1_8.toString()
+        }
+    }
+
+    tasks.withType<Test> {
+        testLogging {
+            showStandardStreams = true
+
+            // set options for log level LIFECYCLE
+            events = setOf(
+                    TestLogEvent.FAILED,
+                    TestLogEvent.PASSED,
+                    TestLogEvent.SKIPPED,
+                    TestLogEvent.STANDARD_OUT
+            )
+
+            exceptionFormat = TestExceptionFormat.FULL
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+
+            // set options for log level DEBUG and INFO
+            debug {
+                events = setOf(
+                    TestLogEvent.STARTED,
+                    TestLogEvent.FAILED,
+                    TestLogEvent.PASSED,
+                    TestLogEvent.SKIPPED,
+                    TestLogEvent.STANDARD_ERROR,
+                    TestLogEvent.STANDARD_OUT
+                )
+
+                exceptionFormat = TestExceptionFormat.FULL
+            }
+
+            info.events = debug.events
+            info.exceptionFormat = debug.exceptionFormat
+        }
+
+        afterSuite(
+            KotlinClosure2({ desc: TestDescriptor, result: TestResult ->
+                if (desc.parent == null) { // will match the outermost suite
+                    println("Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} successes, ${result.failedTestCount} failures, ${result.skippedTestCount} skipped)")
+                }
+            })
+        )
+    }
+
+    extensions.getByType<PublishingExtension>().publications {
+        create<MavenPublication>("maven") {
+            pom {
+                url.set("https://github.com/grpc/grpc-kotlin")
+
+                scm {
+                    connection.set("scm:git:https://github.com/grpc/grpc-kotlin.git")
+                    developerConnection.set("scm:git:git@github.com:grpc/grpc-kotlin.git")
+                    url.set("https://github.com/grpc/grpc-kotlin")
+                }
+
+                licenses {
+                    license {
+                        name.set("Apache 2.0")
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("grpc.io")
+                        name.set("gRPC Contributors")
+                        email.set("grpc-io@googlegroups.com")
+                        url.set("https://grpc.io/")
+                        organization.set("gRPC Authors")
+                        organizationUrl.set("https://www.google.com")
+                    }
+                }
+            }
+        }
+    }
+
+    extensions.getByType<SigningExtension>().sign(extensions.getByType<PublishingExtension>().publications.named("maven").get())
+    extensions.getByType<SigningExtension>().useInMemoryPgpKeys(System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PASSPHRASE"))
+
+    tasks.withType<Sign> {
+        onlyIf { System.getenv("GPG_PRIVATE_KEY") != null }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
+}
